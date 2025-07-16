aea95b0b17c8f1b9440ef5c65a4493c6322c919f
ishika <ishikatakhtani04@gmail.com>
2025-07-16 10:32:18 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import StoreForm from './Pages/StoreForm';
 import ProductSelection from './Pages/ProductSelection';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import Layout from './Layout';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<StoreForm />} />
-         <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        <Route path="/product-selection" element={<ProductSelection/>} />
+        <Route path="/" element={<Layout />}>
+          {/* All these pages will show Footer */}
+          <Route index element={<StoreForm />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="product-selection" element={<ProductSelection />} />
+        </Route>
       </Routes>
     </Router>
   );
